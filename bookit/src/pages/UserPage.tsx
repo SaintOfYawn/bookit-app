c3b215e566874aec85f58bc3cb009b58469dfff0
AzizIo <azizbekmadzitov@gmail.com>
2026-05-16 21:58:21 +0500

    добавлен заголовок(не трогать это додлеаю сам)

--- bookit/src/pages/UserPage.tsx
+++ bookit/src/pages/UserPage.tsx
@@ -1,7 +1,8 @@
 export default function UserPage() {
     return (
         <>
-            <div>
+            <div className="min-h-screen bg-[#0f1629] ">
+                <div className="h1 text-2xl font-semibold py-12 mx-6">My Dashboard</div>
                 
             </div>
         </>
